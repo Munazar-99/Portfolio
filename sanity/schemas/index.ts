08d2d006a49d69c27e70ef0519e214632491c538
Munazar <112558267+Munazar-99@users.noreply.github.com>
2023-04-16 00:15:41 +0200

    Delete sanity directory

--- sanity/schemas/index.ts
+++
@@ -1,8 +0,0 @@
-import blockContent from './blockContent'
-import aboutpage from './aboutpage'
-import homepage from './homepage'
-import projects from './projects'
-import social from './social'
-import skill from './skill'
-
-export const schemaTypes = [homepage, projects, aboutpage, blockContent, social, skill]
