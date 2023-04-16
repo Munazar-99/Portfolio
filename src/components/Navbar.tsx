beec55551c6db3641b1fd5afb9cc6f0fbc8846c1
Munazar-99 <munazar.ali.001@gmail.com>
2023-04-16 12:57:21 +0200

    fixes production bugs on the homepage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -75,7 +75,7 @@ const Navbar = () => {
         <motion.section
         initial={{scale:0, opacity:0, x:'-50%', y: '-50%'}}
         animate={{scale:1, opacity:1}}
-        className='min-w-[70vw] flex flex-col justify-between items-center fixed  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-12'>
+        className='min-w-[70vw] min-h-[60vh]  flex flex-col justify-between items-center fixed  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-12'>
       <nav className='flex items-center flex-col justify-center'>
         <CustomMobileLink href='/' title='Home' className=''/>
         <CustomMobileLink href='/about' title='About' className=''/>
