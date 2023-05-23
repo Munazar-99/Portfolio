1bb2b3184fc440a963df1bd1cdcfc7389dd63343
Munazar-99 <munazar.ali.001@gmail.com>
2023-05-23 22:29:24 +0200

    replaces cv with updated one

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,7 +58,7 @@ export default function Home({ homepage }: Props) {
                 {homepage.introduction.replace('software', `${text}`)}
               </p>
               <div className='flex items-center self-start mt-2 lg:self-center'>
-                <Link href='/CV_Munazar Ali.pdf' target={'_blank'}
+                <Link href='/CV_Munazar-Ali.pdf' target={'_blank'}
                   className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent 
                  hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base  '
                 >Resume  <ArrowTopRightOnSquareIcon className={'w-6 ml-1'} />
