6f8f64dc833c24636c052b0ce377540a818a3688
Munazar Ali <munazar.ali.001@gmail.com>
2023-08-19 12:09:56 +0200

    update change in type name in utility functions

--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -1,9 +1,9 @@
-import { Project } from "../typings"
+import { Tproject } from "../typings"
 
 export const fetchProjects = async () => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`)
     const data = await res.json()
-    const projects: Project[] = data.projects
+    const projects: Tproject[] = data.projects
 
     return projects
 }
