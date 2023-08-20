abb727acef91613567512830d3fb6c20b4d3d73b
Munazar Ali <munazar.ali.001@gmail.com>
2023-08-20 15:41:36 +0200

    add name to hamburger menu button for better accessibility

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light relative z-10 lg:px-16 md:px-12 sm:px-8">
       <button
         onClick={hamburgerClickHandler}
-        name="Hamburger menu"
+        aria-label="Toggle Menu"
         className=" flex-col justify-center items-center hidden lg:flex "
       >
         <span
